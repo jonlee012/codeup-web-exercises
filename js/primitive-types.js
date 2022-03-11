edf0d9c9966740bba5681f865421388abb9a07d6
Kimberly Shim <kimberlyshim@kimberlys-mbp.lan>
2022-03-10 18:29:47 -0600

    exercise complete

---
+++ js/primitive-types.js
@@ -0,0 +1,26 @@
+// DATA TYPES
+
+//primitive type exercise
+// number
+99.9
+0
+// boolean
+false
+true
+
+// string
+'0'
+"false"
+'true'
+
+// undefined
+    // system doesn't find the value
+    // the value hasn't been defined inside of your js progress
+
+//What data type would best represent:
+//A term or phrase typed into a search box? String
+//If a user is logged in? boolean
+//A discount amount to apply to a user's shopping cart? number
+//Whether or not a coupon code is valid? boolean
+//An email address typed into a registration form? string
+//The price of a product? number
