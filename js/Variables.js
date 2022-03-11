066d28a06f8d1f012b889d569ae850be8f62e189
Kimberly Shim <kimberlyshim@kimberlys-mbp.lan>
2022-03-11 16:13:48 -0600

    variable exercise

---
+++ js/Variables.js
@@ -0,0 +1,12 @@
+var name = "jon lee", age = 35,
+
+// var a = 1;
+// var b = a++;
+// var c = ++a;
+//a=1 b=1 c=3
+// what is the value of a, b, and c?
+
+//why is isNaN("")
+//false
+
+var sample = "Hello Codeup";
