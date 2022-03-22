23281a3bb9b8262008afa56f61311662172b8001
Kimberly Shim <kimberlyshim@kimberlys-mbp.lan>
2022-03-22 09:33:13 -0500

    morning warmups

---
+++ js/warmups/morning-warmups.js
@@ -0,0 +1,15 @@
+var side1 = 4
+var side2 = 4
+var side3 = 4
+var s = (side1 + side2 + side3) / 2; var area = Math
+var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+console.log(area)
+
+
+function triangleArea(a, b, c) {
+    var sp = (a + b + c) / 2;
+    var area = Math.sqrt(sp * (sp - a) * (sp - b) * (sp - c));
+    console.log(area);
+}
+
+triangleArea(4, 4, 4);
