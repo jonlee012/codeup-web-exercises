9c5f50e88afc65695abb1ed51172e79b4948f526
Kimberly Shim <kimberlyshim@kimberlys-mbp.lan>
2022-03-27 14:48:14 -0500

    planets string

--- js/arrays/planets-string.js
+++ js/arrays/planets-string.js
@@ -9,22 +9,28 @@
      * console.log planetsArray to check your work
      */
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
     var planetsArray = planetsString.split("|");
 
     console.log(planetsArray);
 
-    /**
-     * TODO:
-     * Create a string with <br> tags between each planet. console.log() your
-     * results. Why might this be useful?
-     *
-     * BONUS:
-     * Create another string that would display your planets in an undordered
-     * list. You will need an opening AND closing <ul> tags around the entire
-     * string, and <li> tags around each planet.
-     */
-    console.log(planetsArray.join("\n"));
+    // TODO:
+// Create a string with <br> tags between each planet. console.log() your
+// results. Why might this be useful?
+//
+
+// var newArray = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+// console.log(newArray);
+
+var  newArray1 = Mercury + " - " + Venus + " <br /> " + Earth + " - " + Mars + " <br /> " + Jupiter + " - " + Saturn + " <br /> " + Uranus + " - " + Neptune;
+console.log(newArray1);
+
+console.log(planetsArray.join("\n"));
+
+// BONUS:
+// Create another string that would display your planets in an undordered list.
+// You will need an opening AND closing <ul> tags around the entire
+// string, and <li> tags around each planet.
+
 
 
 // var brBuffer="";
@@ -44,3 +50,8 @@
 
 
 // })();
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("unshuffled array " + array);
+var shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+console.log("shuffled array " + shuffledArray)
