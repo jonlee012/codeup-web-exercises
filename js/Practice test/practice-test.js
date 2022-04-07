59432454e347ac20bc538b30b38de0e70f2a49e5
jonlee012 <jonlee86.jl@gmail.com>
2022-04-07 09:28:44 -0500

    practice test

--- js/Practice test/practice-test.js
+++ js/Practice test/practice-test.js
@@ -1,4 +1,7 @@
-// 1. Create a function named `calcAge` that takes the age and return the age in days. If input is not numeric, calcAge should return false.
+//
+//
+// 1. Create a function named `calcAge` that takes the age and return the age in days.
+// If input is not numeric, calcAge should return false.
 //
 // function calcAge(aa){
 //     if(typeof(aa) !== isNaN(aa)){
@@ -16,19 +19,21 @@ function calcAge(aa) {
     } else {
         return false
     }
-    return ageDays
+    return ageDays;
 }
-console.log(calcAge('two'));
+console.log(calcAge(2));
 //
-// 2. Create a function named `increment` that takes a number as an argument, increments the number by +1 and returns the result. If input is not numeric, increment should return false.
+// 2. Create a function named `increment` that takes a number as an argument, increments
+// the number by +1 and returns the result. If input is not numeric, increment should return false.
 //
 function increment(bb) {
     if (typeof(bb) === 'number') {
-        var addOne = (++bb);
+        // var addOne = (++bb);
+        var addOne = bb +1;
     } else {
         return false
     }
-    return addOne
+    return addOne;
 }
 console.log(increment(2));
 //
@@ -36,24 +41,40 @@ console.log(increment(2));
 
 
 //
-// 3. Define a function named `isOdd` that takes in a number, return true is that number is odd. Return false if the number is either not a number or even.
+// 3. Define a function named `isOdd` that takes in a number, return true is that number is odd.
+// Return false if the number is either not a number or even.
 //
 function isOdd(cc) {
-    if (typeof(cc) === 'number' && cc % 3 === 0) {
+    if (typeof(cc) === 'number' && cc % 3) {
         return true
     } else {
         return false
     }
 }
-console.log(isOdd('two'));
+console.log(isOdd(1));
 //
-// 4. Write a function named `oddArr`. Given the array of var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers.
+// 4. Write a function named `oddArr`. Given the array of
+// var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers.
+//
+function oddArr(num) {
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return num % 2;
+}
+// oddArr.forEach(number => {
+//     if (number % 3 == 0) {
+//         oddNum.push(number);
+//     }
+// });
+
+console.log(num);
+
+
+// 5. Define a function named `creditCard` that takes a credit card number and
+// only displays the last four characters. The rest of the card number must be replaced by ************.
 //
 //
-// 5. Define a function named `creditCard` that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
-//
-//
-// 6. create a function named `AlphabetSoup` that takes a string and returns a string with its letters in alphabetical order. Return false if string is empty or null
+// 6. create a function named `AlphabetSoup` that takes a string and returns a
+// string with its letters in alphabetical order. Return false if string is empty or null
 //
 //
 // 7. create a function named `reverse` that takes in and reverses an array.
