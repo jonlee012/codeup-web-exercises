59138a7ae87c2e9d58f6cdc41b2e88224010aceb
jonlee012 <jonlee86.jl@gmail.com>
2022-04-18 10:00:57 -0500

    WARMUPS

--- js/warmups/morning-warmups.js
+++ js/warmups/morning-warmups.js
@@ -224,3 +224,28 @@ console.log(combineArrays([1, 2, 3],[3, 4, 5]));
 
 // example input: [{a:1,b:2},{a:5,b:4}]
 // expected output: [{a:1,b:2},{a:5,b:4}]
+
+// Rewrite the following using a for-loop:
+// var i = 20;
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+var
+    for (var i = 20; i <= 0; i--) {
+        console.log(i);
+    }
+
+
+// 2. Write a function that takes an object (a) as argument and returns an array with all object keys
+// example input: {a:1,b:2,c:3}
+// expected output: ['a','b','c']
+// ------------------------
+//     example input:{j:9,i:2,x:3,z:4}
+// expected output: ['j','i','x','z']
+
+var keys = {
+    a:1,b:2,c:3
+};
+
+console.log(Object.keys(keys));
