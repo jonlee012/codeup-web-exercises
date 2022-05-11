de8acf4a0b9c80c9fb2016ab3332d2f76992eeeb
jonlee012 <jonlee86.jl@gmail.com>
2022-05-11 15:06:44 -0500

    solutions

--- JS/solutions.js
+++ JS/solutions.js
@@ -124,3 +124,222 @@ toy = {
     inventory: 50
 }
 console.log(profit(toy));
+
+
+
+
+"use strict";
+
+/**
+ * Write your solutions here
+ *
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ */
+
+// TODO Write a function named isNegative that accepts a number and
+//  returns true or false based on whether the input is negative.
+function isNegative(num) {
+    if (typeof(num) === 'number' && num < 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isNegative(1));
+
+// TODO Write a function named isTen that accepts a number and returns a
+//  boolean that indicates whether or not that number is equal to 10.
+function isTen(num) {
+    if (typeof(num) === 'number' && num === 10) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isTen(1));
+// TODO Write a function named double that accepts a number and returns
+//  the number doubled.
+function double(num) {
+    if (typeof(num) === 'number') {
+        return num * 2
+    }
+}
+console.log(double(1));
+// TODO Write a function named remove9s that accepts an array of numbers
+//  and returns an array with all the same numbers except for 9.
+function remove9s(arr) {
+    var i = 0;
+    while (i < arr.length) {
+        if (arr[i] === 9) {
+            arr.splice(i, 1);
+        } else {
+            ++i;
+        }
+    }
+    return arr;
+}
+// Usage
+console.log(remove9s([2,5,9,1,5,8,5], ))
+
+
+// TODO Write a function named average that accepts an array of numbers and
+//  returns the average of those numbers.
+function average(numArr){
+    var total = 0;
+    var count = 0;
+
+    numArr.forEach(function(item, index) {
+        total += item;
+        count++;
+    });
+
+    return total / count;
+}
+console.log(average([2, 3, 4, 5]));
+// TODO Write a function named countOdds that accepts an array of numbers and
+//  returns the number of odd numbers in the array.
+// function countOdds(numArr) {
+//     var count = 0;
+//     for (var i = 0; i < numArr.length; i++) {
+//
+//         if (typeof (i) === 'number' && num % 2 !== 0) {
+//             count++;
+//         }
+//         return count;
+//     }
+// }
+// console.log(countOdds(1));
+function countOdds(arr) {
+    // var myArray = []
+    var counter = 0
+    for(var i = 0; i < arr.length; i++) {
+
+        if(arr[i] % 2 !== 0) {
+            counter++ }
+    }
+    return counter
+}
+console.log(countOdds(1));
+
+// TODO Write a function named averageSales that accepts an array of objects where
+//  each object represents a person, and has a sales property.
+//  The function should return the average of every object's sales property.
+// function averageSales(arr){
+//     var arrObj = person([
+//         {name: 'Jim Halpert', sales: 100},
+//         {name: 'Dwight Schrute', sales: 50},
+//         {name: 'Andy Bernard', sales: 150},
+//     ])
+//     var salesTotal = arrObj[0].sales +;
+//     var prof = product.inventory * netPrice
+//     return Math.round(prof)
+//
+// }
+// console.log(averageSales(arr));
+function averageSales(list) {
+    var sum = 0;
+    var personCount = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].name == "") {
+            personCount++;
+        } else if (list[i].sales == 'number'){
+            sum += list[i].sales;
+        }
+    }
+    var avg = sum / personCount;
+    return avg;
+}
+
+console.log(averageSales([
+    {name: 'Jim Halpert', sales: 100},
+    {name: 'Dwight Schrute', sales: 50},
+    {name: 'Andy Bernard', sales: 150},
+]));
+
+// TODO Write a function named convertNameToObject that accepts a string that contains
+//  a first name and last name separated by a space character, and
+//  returns an object with properties firstName and lastName.
+function convertNameToObject(string){
+    var newStr = "";
+    for (let i = 0; i < string.length; i++){
+        newStr = JSON.parse(string[i]) + JSON.parse(string[i]);
+    }
+    return newStr
+}
+console.log(convertNameToObject('hello world'));
+
+
+// TODO Write a function named countVowels that accepts a string and returns the
+//  number of vowels in that string. (Don't worry about or count
+//  "y" as a vowel)
+// function countVowels(str) {
+//     var count = 0;
+//     for (let i = 0; i < str.length; i++){
+//         if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u"){
+//             count++;
+//         }
+//         return count;
+//     }
+//     // if (arr.length == 1) {
+//     //     var vowels = new Array("a", "e", "i", "o", "u");
+//     //     var isVowel = false;
+//     //     for (e in vowels) {
+//     //         if (vowels[e] == arr) {
+//     //             isVowel = true;
+//     //         }
+//     //     }
+//     //     return isVowel;
+//     // }
+// }
+// console.log(countVowels("bruisd"));
+// var count = 0;
+// function countVowels(str) {
+//
+//     // find the count of vowels
+//     var count = str.match(/[aeiou]/gi).length;
+//
+//     // return number of vowels
+//     return count;
+// }
+//
+// console.log(countVowels("vsda"));;
+
+// defining vowels
+function countVowels(str) {
+    var vowels = ["a", "e", "i", "o", "u"]
+    var count = 0;
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels());
+
+
+// TODO Write a function named analyzeWord. It should take in a string and return an
+//  object with information about the input word. The object returned should
+//  have the following properties:
+//  word: the original word that was passed into the function
+//  numberOfLetters: the number of letters in the word
+//  numberOfVowels: the number of vowels in the word
+
+
+// TODO Write a function named capitalizeName that accepts a string that is a first and
+//  last name separated by a space, and returns a string that that has the first and
+//  last names capitalized.
+//  For this problem, you may assume that the function will only ever be called with a string that has two words separated by a single space.
+
+function capitalizeName (str){
+    var arr = str.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    var str = arr.join(" ");
+    return str;
+}
+console.log(capitalizeName("hello world"));
