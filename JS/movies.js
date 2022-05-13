079de049a6fab013c66f55ed8e78f30cd016bd2f
jonlee012 <jonlee86.jl@gmail.com>
2022-05-13 14:27:10 -0500

    movies

--- JS/movies.js
+++ JS/movies.js
@@ -1,99 +1,311 @@
-"use strict"
-var coffees = [];
-function fillCoffeeArray()
-{
-    if (localStorage.getItem("coffeeArray") === null) {
-        coffees = [
-            {id: 1, name: 'Light City', roast: 'light'},
-            {id: 2, name: 'Half City', roast: 'light'},
-            {id: 3, name: 'Cinnamon', roast: 'light'},
-            {id: 4, name: 'City', roast: 'medium'},
-            {id: 5, name: 'American', roast: 'medium'},
-            {id: 6, name: 'Breakfast', roast: 'medium'},
-            {id: 7, name: 'High', roast: 'dark'},
-            {id: 8, name: 'Continental', roast: 'dark'},
-            {id: 9, name: 'New Orleans', roast: 'dark'},
-            {id: 10, name: 'European', roast: 'dark'},
-            {id: 11, name: 'Espresso', roast: 'dark'},
-            {id: 12, name: 'Viennese', roast: 'dark'},
-            {id: 13, name: 'Italian', roast: 'dark'},
-            {id: 14, name: 'French', roast: 'dark'},
-        ];
-    }
-    else
-    {
-        coffees = JSON.parse(localStorage.getItem("coffeeArray"));
-    }
-    coffeeToPage(coffees);
-}
-function coffeeToPage(coffeeArray)
-{
-    tbody.innerHTML = renderCoffees(coffeeArray.sort((a,b) => (a.name > b.name) ? -1 : 1));
-}
-function saveCoffeeArray()
-{
-    localStorage.setItem("coffeeArray",JSON.stringify(coffees));
-}
-function addNewCoffee()
-{
-    var newCoffeeName = addCoffee.value;
-    var selectedRoast = roastSelectionNew.value;
-    var obj = {};
-    obj['id'] = coffees.length+1;
-    obj['name'] = newCoffeeName;
-    obj['roast'] = selectedRoast;
-    coffees.push(obj);
-    saveCoffeeArray();
-}
-function renderCoffee(coffee) {
-    var html = '<div class="card tr coffee">';
-    html += '<div class="card-body coffeeCard">';
-    html += '<div class="td coffeeName " style="max-width:150px;">' + coffee.name + '</div>';
-    html += '<div class="td coffeeRoast text-muttext-muteded">' + coffee.roast + '</div>';
-    html += '</div>';
-    html += '</div>';
-    return html;
-}
-function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
-function updateCoffees(e) {
-    var textSearch = nameSelection.value.toLowerCase();
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    console.log(textSearch);
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        } else if (selectedRoast === "any") {
-            filteredCoffees.push(coffee);
-        }
-    });
-    coffeeToPage(filteredCoffees);
-}
-function updateCoffeesText(e) {
-    var filteredCoffees = [];
-    var textSearch = nameSelection.value.toLowerCase();
-    var selectedRoast = roastSelection.value;
-    console.log(textSearch);
-    coffees.forEach(function(coffee) {
-        if (coffee.name.toLowerCase().includes(textSearch) && (coffee.roast.toLowerCase() === selectedRoast || selectedRoast === 'any')) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    coffeeToPage(filteredCoffees);
-}
-var tbody = document.querySelector('#coffees');
-tbody.innerHTML = renderCoffees(coffees);
-var submitButton = document.querySelector('#submit');
-var submitButtonText = document.querySelector('#submitText');
-var roastSelection = document.querySelector('#roast-selection');
-var nameSelection = document.querySelector("#submitType")
-var submitCoffee = document.querySelector("#submitCoffee")
-submitCoffee.addEventListener('click', addNewCoffee);
+// const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
+// const url = '/posts';
+// const options = {
+//     method: 'POST',
+//     data: {
+//         'title': '',
+//     },
+//     body: JSON.stringify(blogPost),
+// };
+
+// RELOAD BUTTON
+
+const log = document.querySelector('.event-log-contents');
+const reload = document.querySelector('#reload');
+
+reload.addEventListener('click', () => {
+    log.textContent ='';
+    window.setTimeout(() => {
+        window.location.reload(true);
+    }, 200);
+});
+
+window.addEventListener('load', (event) => {
+    log.textContent = log.textContent + 'load\n';
+});
+
+const url = 'https://spectacular-hammerhead-galley.glitch.me/movies'
+fetch('https://spectacular-hammerhead-galley.glitch.me/movies')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        // const example = data;
+        // console.log(example);
+        data.forEach(post => {
+            if (typeof (post.id) === 'number' || typeof (post.id) === 'string') {
+                console.log(post.title);
+
+                // function hasTitle(){
+                //     if(post.title !== 'undefined'){
+                $('#movies').append(
+                    `<div class="container">` +
+                    `<div class="row" style="outline: auto; padding-bottom: 10px; padding-top: 10px">` +
+                    `<div class="col-4">` +
+                    `<img src="${post.poster}" style="height:300px; width:200px;">` +
+                    `</div>` +
+
+                    `<div class="col-8">` +
+
+                    `<h3>${post.title}</h3>` +
+                    `<ul style="list-style: none">` +
+                    `<li>Genre:    ${post.genre}</li>` +
+                    `<li>Rating:  ${post.rating}</li>` +
+                    `<li>Director:  ${post.director}</li>` +
+                    `<li>Plot:  ${post.plot}</li>` +
+                    `</ul>`+
+                    `<button id="${post.id}" >delete movie</button>` +
+                    `</div>` +
+                    `</div>` +
+
+                    `</div>`
+
+                )
+            }
+
+        })
 
 
+        //DELETE BUTTON
+
+        console.log(data.length)
+        for (let i = 0; i < data.length; i++) {
+            console.log(data.length)
+            var test = document.getElementById(`${data[i].id}`);
+            test.onclick = function deleteMovie(e) {
+                // e.preventDefault();
+                console.log(data[i].id);
+                fetch(url + "/" + `${data[i].id}`, {
+                    method: 'DELETE'
+                }).then(() => {
+                    console.log('removed');
+                    window.location.reload(true);
+                })
+                    .catch(err => {
+                        console.error(err);
+                    })
+
+            }
+
+            //         // })
+
+
+            //EDIT BUTTON
+
+            $('#edit').click((e) => {
+                // e.preventDefault();
+
+                var editMovie = {
+                    currentTitle: $("#oldTitle").val(),
+                    title: $("#editTitle").val(),
+                    genre: $("#editGenre").val(),
+                    rating: $("#editRating").val(),
+                    director: $("#editDirector").val(),
+                    plot: $("#editPlot").val(),
+                    poster: $("#editImg").val()
+                }
+                var titleEntered = editMovie.currentTitle
+                // console.log(editMovie.title);
+                //     console.log("You can edit the movie")
+
+                if (titleEntered.toLowerCase() === (data[i].title).toLowerCase()) {
+                    console.log("The same title")
+                    console.log(data[i].id)
+
+                    // // const url = 'https://codeup-json-server.glitch.me/movies';
+                    let options = {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(editMovie),
+                    };
+                    fetch(url + "/" + `${data[i].id}`, options)
+                        .then((response) => {
+                            // console.log(response)
+                            console.log('New rating is: ' + editMovie.rating);
+                            window.location.reload(true)
+                        })
+                        // .then(response => console.log(response)) /* review was created successfully */
+                        .catch(error => console.error(error)); /* handle errors */
+
+                }
+            })
+            console.log(data)
+
+            // TRIED FOR EDIT BUTTON
+
+            // for (let i = 0; i < data.length; i++) {
+            //     console.log(data.length)
+            //     var edit = document.getElementById(`${data[i].id}`);
+            //     edit.onclick = function editMovie(e) {
+            //         // e.preventDefault();
+            //         console.log(data[i].id);
+            //         fetch(url + "/" + `${data[i].id}`, {
+            //             method: 'PUT'
+            //         }).then(() => {
+            //             console.log('edited');
+            //             window.location.reload(true)
+            //         })
+            //             .catch(err => {
+            //                 console.error(err)
+            //             })
+            //
+            //     }
+            //     //         // })
+            // }
+        }});
+//     `<button onclick.(console.log($("#"+`${data[i].id}`)))>`;
+//             $("#"+"`${data[i].id}`").click(
+//             if ("`${data[i].id}`") {
+//
+// //DELETE request
+//                     fetch(url + "/" + `${data[i].id}`, {
+//                         method: 'DELETE'
+//                     }).then(() => {
+//                         console.log('removed');
+//                     }).catch(err => {
+//                         console.error(err)
+//                     })
+//
+//                 })
+//             }
+
+
+//         for (let i = 0; i < data.length; i++) {
+//             if ($("#"+`${data[i].id}`).click) {
+//
+// //DELETE request
+//                     fetch(url + "/" + `${data[i].id}`, {
+//                         method: 'DELETE'
+//                     }).then(() => {
+//                         console.log('removed');
+//                     }).catch(err => {
+//                         console.error(err)
+//                     })
+//
+//                 }
+//             }
+
+//         (function (id) {
+// //DELETE request
+//                     fetch(url + "/" + `${post.id}`, {
+//                         method: 'DELETE'
+//                     }).then(() => {
+//                         console.log('removed');
+//                     }).catch(err => {
+//                         console.error(err)
+//                     })
+//
+//             })
+//     }
+// }
+// return hasTitle()
+
+// })
+
+// function addMovie() {
+//     const reviewObj = {
+//         id: movies.length + 1,
+//         title: document.getElementById(),
+//         director: 'Codey',
+//         rating: 5,
+//         genre: "This is a really good place for coding and eating"
+//     };
+//     const url = 'https://codeup-json-server.glitch.me/movies';
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(reviewObj),
+//     };
+//     fetch(url, options)
+//         .then(response => console.log(response)) /* review was created successfully */
+//         .catch(error => console.error(error)); /* handle errors */
+// }
+
+
+$('#add').click((e) => {
+    e.preventDefault();
+
+    var addMovie = {
+        title: $("#title").val(),
+        genre: $("#genre").val(),
+        rating: $("#rating").val(),
+        director: $("#director").val(),
+        plot: $("#plot").val(),
+        poster: $("#addImg").val()
+    }
+    // const url = 'https://codeup-json-server.glitch.me/movies';
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(addMovie),
+    };
+    fetch(url, options)
+        .then((response) => {
+            console.log(response)
+            // console.log('removed');
+            window.location.reload(true)
+        })
+        // .then(response => console.log(response)) /* review was created successfully */
+        .catch(error => console.error(error)); /* handle errors */
+
+})
+
+
+// let deleteOptions = {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     }
+// };
+
+
+// $("#delete").click(function() {
+// //DELETE request
+//     fetch(`${url}/${inputVal}`, deleteOptions)
+//         .then();
+// });
+
+// $('#delete').click((e) => {
+//     e.preventDefault();
+
+// let options = {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(),
+// };
+// fetch(url, options)
+//     .then(response => console.log(response)) /* review was created successfully */
+//     .catch(error => console.error(error)); /* handle errors */
+// })
+
+
+// $('button').click($(this) => console.log($(this).siblings))
+
+// const xhr = new XMLHttpRequest()
+// //open a get request with the remote server URL
+// xhr.open("GET", "https://codeup-json-server.glitch.me/movies")
+//
+// //send the Http request
+// xhr.send()
+//
+// //EVENT HANDLERS
+//
+// //triggered when the response is completed
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         //parse JSON datax`x
+//         data = JSON.parse(xhr.responseText)
+//         console.log(data)
+//         console.log(typeof(data.title))
+//         console.log(data.rating)
+//     } else if (xhr.status === 404) {
+//         console.log("No records found")
+//     }
+// }
