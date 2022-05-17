7994955492569a7b2b8aed431435b33a3e6eebfa
jonlee012 <jonlee86.jl@gmail.com>
2022-05-17 16:26:39 -0500

    keys

--- JS/keys.js
+++ JS/keys.js
@@ -1,6 +1,6 @@
 
 
-
+const GITHUB_TOKEN = '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';
 
 const MAPBOX_KEY = 'pk.eyJ1Ijoiam9ubGVlMDEyIiwiYSI6ImNsMnQ3OXZvazAwcnozY211dHJwcDFqZXYifQ.a6OSzHZ5fwhVc3HiA3hycw';
 const OPEN_WEATHER_APPID = "8dba1fe72474760cbb96da7c28d67b5e";
