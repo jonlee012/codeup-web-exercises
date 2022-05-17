0e82ca0df2396680b0885476d02c6aae89793697
jonlee012 <jonlee86.jl@gmail.com>
2022-05-17 16:16:26 -0500

    js with html

--- JS/js-with-html-practice.js
+++ JS/js-with-html-practice.js
@@ -1,6 +1,34 @@
-var myName = "Jon";
-var myAge = 35;
-var isMonday = true;
-console.log(myName, myAge);
-console.log("today is monday:", isMonday)
-console.log("my Name: " + myName + " my age: " + myAge); //concatenation
+// var myName = "Jon";
+// var myAge = 35;
+// var isMonday = true;
+// console.log(myName, myAge);
+// console.log("today is monday:", isMonday)
+// console.log("my Name: " + myName + " my age: " + myAge); //concatenation
+
+// if (true) {
+//     let x = 5;
+// }
+// console.log(typeof x);
+
+// function sayHello(name) {
+//     return 'Hello, ' + name + '!';
+// }
+//
+// console.log(sayHello('jon'));
+
+// const sayHello = (name) => { 'Hello, ' + name + '!' }
+// console.log((sayHello('jon')));
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.map(n => n * 3));
+// // console.log(n);
+
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+// console.log(numbers);
+
+// var myOutput = originalString + " is an example of " + description + " thinking.";
