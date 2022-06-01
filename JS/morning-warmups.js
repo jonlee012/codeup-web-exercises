f985bc54abdff06739b8d5d7c91cf63f8dc7b9d7
jonlee012 <jonlee86.jl@gmail.com>
2022-06-01 09:42:57 -0500

    warmups

--- JS/morning-warmups.js
+++ JS/morning-warmups.js
@@ -553,3 +553,44 @@ function splitString (param){
 }
 
 console.log(splitString("hello"));
+
+
+// Create a function that takes the age in years and
+// returns the age in days.
+//     example input: 65
+// expected output: 23725
+
+function ageToDays (age){
+    return age * 365
+}
+
+console.log(ageToDays(65));
+
+// Create a function that takes two numbers and a
+// mathematical operator + - / * and will perform a
+// calculation with the given numbers.
+// example inputs: 4, '/', 2
+// expected output: 2
+var operations = {
+    "+" : function (operand1, operand2) {
+        return operand1 + operand2;
+    },
+    "-" : function (operand1, operand2) {
+        return operand1 - operand2;
+    },
+    "*" : function (operand1, operand2) {
+        return operand1 * operand2;
+    },
+    "/" : function (operand1, operand2) {
+        return operand1 / operand2;
+    }
+};
+
+function accumulate(list, operator) {
+    return list.reduce(operations[operator]);
+}
+
+console.log(accumulate([4, 2], "+"));     // 10
+console.log(accumulate([4, 2], "-"));     // -8
+console.log(accumulate([4, 2], "*"));     // 24
+console.log(accumulate([4, 2], "/"));     // 24
